4e4e84197c9f7563d471fabc953529aaffd0d022
Ajay <161434016+ajaykumar1806@users.noreply.github.com>
2025-07-23 14:53:46 +0530

    Add files via upload

--- lms_assignments/loops2_patterns/3.c
+++ lms_assignments/loops2_patterns/3.c
@@ -7,8 +7,10 @@ int main() {
         for(int i =1;i<=n;i++) {
                 char c = ch;
                 for(int j = 1;j<=n;j++) {
-			if(i+j > n)
-                        	(j == n) ? printf("%c",c++) : printf("%c*",c++);
+			if(i+j > n) {
+                        	(j == n) ? printf("%c",c) : printf("%c*",c);
+				c++;
+			}
 			else 
 				printf(" ");
                 }
